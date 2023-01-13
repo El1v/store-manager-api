3978a04a360c538d4e6086d2246e7bd797657475
Elivelton <eliveltonmn@outlook.com>
2023-01-13 19:49:38 -0300

    fix: lint

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,11 +1,8 @@
-const camelize = require('camelize');
+// const camelize = require('camelize');
 const snakeize = require('snakeize');
 const connection = require('./connection');
 
 const insert = async (sales) => {
-  sales.map((sale) => {
-
-  });
   const columns = Object.keys(snakeize(sales)).join(', ');
 
   const placeholders = Object.keys(sales)
